a7da8813f1aed117c201334665830f84a9ed60d4
moofat7y <mohammedbelal802@gmail.com>
2023-06-12 01:46:14 +0300

    third commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://plant-diseases-h2mk.onrender.com/api/";
+const baseUrl = "https://plant-diseases-2rdp.onrender.com/api/";
 
 const api = axios.create({ baseURL: baseUrl, withCredentials: true });
 api.interceptors.request.use(
