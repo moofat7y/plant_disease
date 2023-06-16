d9b011943db7f48cbc33a89b3ad4ed8b7bc9c724
moofat7y <mohammedbelal802@gmail.com>
2023-06-16 19:15:23 +0300

    third commit

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -1,18 +1,22 @@
-const User = require("../models/userModel");
 const { uploadImg } = require("../utils/cloudUpload");
+const Result = require("../models/detectionModel");
 const fs = require("fs");
 exports.saveImage = async (req, res, next) => {
   const image = req.file;
+  const { type, diagnose, confidance } = req.body;
   try {
     const uploader = (path) => uploadImg(path, "detection");
 
     const path = await uploader(image.path);
 
     fs.unlinkSync(image.path);
-    const user = await User.findByIdAndUpdate(req.user._id, {
-      $push: { "predImg.images": path, "predImg.result": { class: "disesed" } },
-    });
-    res.status(201).json(path);
+    let result = await Result.findOne({ userId: req.user._id });
+    if (!result) {
+      result = await Result.create({ userId: req.user._id });
+    }
+    result.results.push({ type, confidance, image: path, diagnose });
+    await result.save();
+    res.status(201).json({ message: "Result saved" });
   } catch (error) {
     next(error);
   }
--- server/models/detectionModel.js
+++ server/models/detectionModel.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var predSchema = new mongoose.Schema({
+const predSchema = new mongoose.Schema({
   results: [
     {
       confidance: { type: Number, required: true },
-      image: { type: String, required: true },
-      diagonse: { type: String, required: true },
+      type: { type: String, required: true },
+      image: { type: Object, required: true },
+      diagnose: { type: String, required: true },
     },
   ],
   userId: { type: mongoose.Types.ObjectId, ref: "User" },
